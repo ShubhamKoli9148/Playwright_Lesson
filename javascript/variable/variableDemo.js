a0307822544e265a92ec66edc4058e075918e9c8
ShubhamKoli9148 <94375776+ShubhamKoli9148@users.noreply.github.com>
2026-09-06 16:38:22 +0530

    added variable defination on variableDemo.js

--- javascript/variable/variableDemo.js
+++ javascript/variable/variableDemo.js
@@ -8,3 +8,6 @@ console.log("Name : "+name);
 console.log("City : "+city);
 console.log("Country : "+country);
 }
+//what is variable in javascript
+// variable is container that holds data values. In JavaScript, we can declare a variable using var, let, or const keywords.
+// it is used to store data that can be used and manipulated throughout the program. Variables can hold different types of data such as numbers, strings, objects, arrays, and more.
