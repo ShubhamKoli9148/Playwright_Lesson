314d77a27a5a164380b974212744094f6be4b74b
ShubhamKoli9148 <94375776+ShubhamKoli9148@users.noreply.github.com>
2026-09-06 15:32:26 +0530

    Variable lesson for js

---
+++ javascript/variable/variableDemo.js
@@ -0,0 +1,10 @@
+{
+    let name = "shubham Koli";
+var city = "Mumbai";
+const country ="India";
+
+city  = "US";
+console.log("Name : "+name);
+console.log("City : "+city);
+console.log("Country : "+country);
+}
